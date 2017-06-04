1b4039668be90a94b7bb7b4849e81df017096af3
himanshu <himanshuchawla2014@gmail.com>
2017-06-03 19:22:07 -0700

    removed moment library

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express=require('express')
-var moment=require('moment')
+
 var app=express()
+var api = require('./time.js');
 app.use('/static',express.static(__dirname + '/static'))
 app.set('views', __dirname + '/views');
 app.set('view engine','ejs')
@@ -10,6 +11,7 @@ app.set('port', (process.env.PORT || 5000));
 app.get('/',function(req,res){
     res.render('index.ejs')
 })
+api(app)
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
